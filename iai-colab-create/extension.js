03ca76b41b4c0f4101f3d99ae112c7e4a4c11bb8
eun-chae-s <s9710173@naver.com>
2023-06-25 13:37:55 -0400

    Fix error

--- iai-colab-create/extension.js
+++ iai-colab-create/extension.js
@@ -8,36 +8,29 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
+async function activate(context) {
   console.log(
     'Congratulations, your extension "iai-colab-create" is now active!'
   );
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
     "iai-colab-create.askGPT",
     async function () {
-      // Get the active notebook document or create a new one if none is active
       let notebook = vscode.window.activeNotebookEditor?.document;
       if (!notebook) {
-          vscode.window.showNotebookDocument({ viewColumn: vscode.ViewColumn.One }).then(newNotebook => {
-              notebook = newNotebook;
-              handleNotebookDocument(notebook);
-          }).catch(err => {
-              vscode.window.showErrorMessage('Failed to create a new notebook document: ' + err);
+        createNewNotebook()
+          .then((newNotebook) => {
+            notebook = newNotebook;
+            handleNotebookDocument(notebook);
+          })
+          .catch((err) => {
+            vscode.window.showErrorMessage(
+              "Failed to create a new notebook document: " + err
+            );
           });
       } else {
-          handleNotebookDocument(notebook);
+        handleNotebookDocument(notebook);
       }
     }
   );
@@ -45,8 +38,41 @@ function activate(context) {
   context.subscriptions.push(disposable);
 }
 
-function handleNotebookDocument(notebook) {
-  // The code you place here will be executed every time your command is executed
+async function createNewNotebook() {
+  const notebookUri = vscode.Uri.parse("untitled:Untitled.ipynb");
+  const content = new Uint8Array(
+    Buffer.from(
+      JSON.stringify({
+        cells: [],
+        metadata: {
+          kernelspec: {
+            display_name: "Python 3",
+            language: "python",
+            name: "python3",
+          },
+          language_info: {
+            codemirror_mode: {
+              name: "ipython",
+              version: 3,
+            },
+            file_extension: ".py",
+            mimetype: "text/x-python",
+            name: "python",
+            nbconvert_exporter: "python",
+            pygments_lexer: "ipython3",
+            version: "3.7.6",
+          },
+        },
+        nbformat: 4,
+        nbformat_minor: 2,
+      })
+    )
+  );
+
+  return vscode.workspace.openNotebookDocument(notebookUri, { content });
+}
+
+async function handleNotebookDocument(notebook) {
   const prompt = await vscode.window.showInputBox({
     prompt: "Enter your prompt: ",
   });
@@ -65,44 +91,46 @@ function handleNotebookDocument(notebook) {
     const generatedText = response.data.choices[0].message.content;
     vscode.window.showInformationMessage(generatedText);
 
-    // Get the active cell
     const activeCell = vscode.window.activeNotebookEditor?.selection?.active;
     if (!activeCell) {
-        vscode.window.showErrorMessage('No active cell found.');
-        return;
+      vscode.window.showErrorMessage("No active cell found.");
+      return;
     }
 
-    // Get the active notebook URI
     const notebookUri = notebook.uri;
-
-    // Create a new cell with the selected text
     const newCell = {
-        cellKind: vscode.CellKind.Code,
-        source: generatedText,
+      cellKind: vscode.CellKind.Code,
+      source: generatedText,
+      language: "python",
     };
 
-    // Add the new cell to the active notebook document
-    vscode.workspace.openNotebookDocument(notebookUri).then(notebookDocument => {
+    vscode.workspace
+      .openNotebookDocument(notebookUri)
+      .then((notebookDocument) => {
         const edit = new vscode.WorkspaceEdit();
         const lastCellIndex = notebookDocument.cells.length;
-        edit.replaceNotebookCells(notebookUri, new vscode.NotebookRange(lastCellIndex, lastCellIndex), [newCell]);
+        const cellRange = new vscode.NotebookCellRange(
+          lastCellIndex,
+          lastCellIndex
+        );
+        edit.replaceNotebookCells(notebookUri, cellRange, [newCell]);
         return vscode.workspace.applyEdit(edit);
-    }).then(success => {
+      })
+      .then((success) => {
         if (success) {
-            vscode.window.showInformationMessage('Text pasted to a new cell.');
+          vscode.window.showInformationMessage("Text pasted to a new cell.");
         } else {
-            vscode.window.showErrorMessage('Failed to paste text to a new cell.');
+          vscode.window.showErrorMessage("Failed to paste text to a new cell.");
         }
-    }).catch(err => {
-        vscode.window.showErrorMessage('An error occurred while pasting text to a new cell: ' + err);
-    });
-    
-    
+      })
+      .catch((err) => {
+        vscode.window.showErrorMessage(
+          "An error occurred while pasting text to a new cell: " + err
+        );
+      });
   } catch (error) {
     console.error(error);
-    vscode.window.showErrorMessage(
-      "An error occurred while generating text."
-    );
+    vscode.window.showErrorMessage("An error occurred while generating text.");
   }
 }
 
